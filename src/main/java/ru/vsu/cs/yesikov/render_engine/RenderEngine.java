05246c1ab998f899df17140d931a233413176881
Yesikov <esikov.2005@gmail.com>
2025-01-07 23:08:49 +0300

    init

--- src/main/java/ru/vsu/cs/yesikov/render_engine/RenderEngine.java
+++ src/main/java/ru/vsu/cs/yesikov/render_engine/RenderEngine.java
@@ -40,10 +40,9 @@ public class RenderEngine {
             ArrayList<Point2f> resultPoints = new ArrayList<>();
             for (int vertexInPolygonInd = 0; vertexInPolygonInd < nVerticesInPolygon; ++vertexInPolygonInd) {
                 Vector3f vertex = mesh.vertices.get(mesh.polygons.get(polygonInd).getVertexIndices().get(vertexInPolygonInd));
+                Vector3f vertexVectorMath = new Vector3f(vertex.getX(), vertex.getY(), vertex.getZ());
 
-                Vector3f vertexVecmath = new Vector3f(vertex.getX(), vertex.getY(), vertex.getZ());
-
-                Point2f resultPoint = vertexToPoint(multiplyMatrix4ByVector3(modelViewProjectionMatrix, vertexVecmath), width, height);
+                Point2f resultPoint = vertexToPoint(multiplyMatrix4ByVector3(modelViewProjectionMatrix, vertexVectorMath), width, height);
                 resultPoints.add(resultPoint);
             }
 
