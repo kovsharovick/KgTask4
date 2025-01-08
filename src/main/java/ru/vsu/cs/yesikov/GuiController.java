e375790c1b042eb48c0868f734eb2f6be00bd8ad
Анастасия Долгих <dolgikh.nastyusha05@gmail.com>
2025-01-08 20:40:37 +0300

    right render and gui

--- src/main/java/ru/vsu/cs/yesikov/GuiController.java
+++ src/main/java/ru/vsu/cs/yesikov/GuiController.java
@@ -60,7 +60,6 @@ public class GuiController {
     @FXML
     public RadioButton radioButtonShades;
     @FXML
-    public RadioButton radioButtonSolidColor;
     private boolean isRotationActive;
     private final Vector2f currentMouseCoordinates = new Vector2f(0, 0);
     private final Vector2f centerCoordinates = new Vector2f(0, 0);
@@ -74,9 +73,6 @@ public class GuiController {
 
     @FXML
     private void initialize() {
-        camera.movePosition(new Vector3f(0, 0, -50));
-        anchorPane.prefWidthProperty().addListener((ov, oldValue, newValue) -> canvas.setWidth(newValue.doubleValue()));
-        anchorPane.prefHeightProperty().addListener((ov, oldValue, newValue) -> canvas.setHeight(newValue.doubleValue()));
 
         Timeline timeline = new Timeline();
         timeline.setCycleCount(Animation.INDEFINITE);
@@ -88,6 +84,8 @@ public class GuiController {
                 new Vector3f(0, 0, 0),
                 1.0F, 1, 0.01F, 100), TRANSLATION));
         scene.setCurrentCameraController(scene.getCameraControllers().get(0));
+
+        scene.getCameraNames().add("Camera №0");
 
         KeyFrame frame = new KeyFrame(Duration.millis(15), event -> {
             double width = canvas.getWidth();
@@ -116,7 +114,11 @@ public class GuiController {
 
         timeline.getKeyFrames().add(frame);
         timeline.play();
+
+        canvas.getOnMouseMoved();
+        canvas.requestFocus();
     }
+
     public void rotateCamera() {
         centerCoordinates.setX((float) (canvas.getWidth() / 2));
         centerCoordinates.setY((float) (canvas.getHeight() / 2));
@@ -135,8 +137,9 @@ public class GuiController {
     }
     public boolean[] getRenderWayData() {
         return new boolean[] {radioButtonMesh.isSelected(), radioButtonShades.isSelected(),
-                radioButtonTexture.isSelected(), radioButtonSolidColor.isSelected()};
+                radioButtonTexture.isSelected()};
     }
+
 
     @FXML
     private void onOpenModelMenuItemClick() {
@@ -149,18 +152,14 @@ public class GuiController {
             return;
         }
 
-        Path fileName = Path.of(file.getAbsolutePath());
-
+        Path filePath = Path.of(file.getAbsolutePath());
         try {
-            String fileContent = Files.readString(fileName);
-            mesh = ObjReader.read(fileContent);
-            mesh.recalculateNormals();
-            Model.triangulate(mesh);
-            // todo: обработка ошибок
-        } catch (IOException exception) {
-
+            ModifiedModel loadedModel = new ModifiedModel(ObjReader.read(Files.readString(filePath)));
+            loadedModel.recalculateNormals();
+            Model.triangulate(loadedModel);
+            scene.getModels().add(loadedModel);
         } catch (Exception e) {
-            throw new RuntimeException(e);
+//            RuntimeException(e);
         }
     }
 
